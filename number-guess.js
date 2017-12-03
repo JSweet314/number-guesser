e920104d577cf80898b9ec788ec35f16c38a03b7
Jon Sweet <jonathan.h.sweet@gmail.com>
2017-12-03 15:31:21 -0700

    added logic to print error if guess out of bounds, disabled submit and clear buttons if guess is correct, forcing user to hit reset button.

--- number-guess.js
+++ number-guess.js
@@ -1,18 +1,17 @@
+// PRODUCTION LOG
 // Phase Two: More Better
 
-// The input field should only accept numerical entries, within the defined min and max range - INPUT FIELD ONLY ACCEPTS NUMERICAL INPUT (changed type from 'submit' to 'number') BUT USER IS STILL ABLE TO MANUALLY TYPE NUMBER OUTSIDE OF PRESET MIN/MAX.
+// The input field should only accept numerical entries, within the defined min and max range - IN PROGRESS - INPUT FIELD ONLY ACCEPTS NUMERICAL INPUT (changed type from 'submit' to 'number') BUT USER IS STILL ABLE TO MANUALLY TYPE NUMBER OUTSIDE OF PRESET MIN/MAX.
 
-// The application should display an error message if the guess is not a number (e.g. parseInt() returns NaN). - USER IS UNABLE TO SUBMIT EMPTY FIELD OR NaN - ISSUE STILL EXISTS THAT USER CAN INPUT NUMBER OUTSIDE PRESET MIN/MAX.
+// The application should display an error message if the guess is not a number (e.g. parseInt() returns NaN). - IN PROGRESS - USER IS UNABLE TO SUBMIT EMPTY FIELD OR NaN - ISSUE STILL EXISTS THAT USER CAN INPUT NUMBER OUTSIDE PRESET MIN/MAX.
 
-// The application should display an error if the guess is outside of the range of possible answers.
+// The application should display an error if the guess is outside of the range of possible answers. - IN PROGRESS - IF USER INPUT IS OUTSIDE OF PRESET RANGE, DESCRIPTION VARIABLE UPDATED TO PRINT ERROR MESSAGE TO SCREEN 
 
 // The clear button should be disabled if there is nothing to clear.  - DONE - IF USER DOES NOT PROVIDE INPUT TO NUMBER FIELD OR CLEAR/RESET BUTTON IS PRESSED AND TEXT FIELD IS CLEARED, CLEAR BUTTON IS DISABLED.
 
 // The reset button should be disabled if there is nothing to reset. - DONE - RESET BUTTON VISIBILILITY PROPERTY SET TO HIDDEN UNTIL GUESS IS MADE AND AFTER RESET BUTTON IS CLICKED
 
-//Research keyword 'this'
-
-
+// Research keyword 'this', error user promtp options for numbers submitted outside of preset range.
 
 var guess = 0;
 var numberOfGuesses = 0;
@@ -27,33 +26,41 @@ sub.addEventListener('click', submitGuess)
 cl.addEventListener('click', clearInput)
 res.addEventListener('click', reset)
 
-//Below - eventListener for user input into text area
+// below - eventListener for user input into text area
 var userInput = document.querySelector('#guess');
 
 userInput.addEventListener('input', enableButtons, false);
 
-//function for eventListener 'input' in text box.
+
 function enableButtons() {
+  //function for eventListener 'input' in text box.
   if (!isNaN(this.value)){ 
     document.querySelector('#submit').disabled = false;
     document.querySelector('#clearText').disabled = false;
-    document.querySelector('#game').setAttribute('class', 'hover');
-    document.querySelector('#submit').setAttribute('class', 'buttonEnabled');
-    document.querySelector('#clearText').setAttribute('class', 'buttonEnabled');
+    changeAttribute('#game', 'class', 'hover');
+    changeAttribute('#submit', 'class', 'buttonEnabled');
+    changeAttribute('#clearText', 'class', 'buttonEnabled');
   } else if (isNaN(this.value) || ((this.value > this.max) || (this.value < this.min))){
     console.log('value isNaN line 39');
-    document.querySelector('#submit').setAttribute('class', 'buttonDisabled');
-    document.querySelector('#clearText').setAttribute('class', 'buttonDisabled');
+    changeAttrubute('#submit', 'class', 'buttonDisabled');
+    changeAttribute('#clearText', 'class', 'buttonDisabled');
   }
 }
 
+if (description === 'BOOM!'){
+  changeAttribute('#submit', 'class', 'buttonDisabled');
+  document.querySelector('#submit').disabled = true;
+}
+
+function changeAttribute(selector, attribute, value) { //function to change element attributes easier
+  toBeChanged = document.querySelector(selector);
+  toBeChanged.setAttribute(attribute, value);
+}
 
 function submitGuess(event) {
   console.log('default form settings prevented');
   console.log('submit guess function');
   numberGuesser();
-  // presentGuess();
-  // describeGuess();
   event.preventDefault();
 }
 
@@ -64,8 +71,10 @@ function numberGuesser(){
   console.log('numberOfGuesses = ' + numberOfGuesses);
   console.log('guess = ' + guess);
   console.log('ans = ' + ans);
-  // presentGuess();
-  if (guess === ans){
+  if ((guess > document.querySelector('#guess').max)||(guess < document.querySelector('#guess').min)){
+    description = "You must submit a number between " + document.querySelector(
+    '#guess').min + " and " + document.querySelector('#guess').max + ".";
+  } else if (guess === ans){
     description = 'BOOM!';
   } else if (guess < ans) {
     description = 'That is too low'
@@ -73,6 +82,14 @@ function numberGuesser(){
     description = 'That is too high';
   }
   describeGuess();
+  if (description === 'BOOM!'){
+  changeAttribute('#submit', 'class', 'buttonDisabled');
+  document.querySelector('#submit').disabled = true;
+  console.log('submit button disabled');
+  changeAttribute('#clearText', 'class', 'buttonDisabled');
+  document.querySelector('#clearText').disabled = true;
+  console.log('clear button disabled');
+  }
   return description;
 }
 
@@ -114,7 +131,6 @@ function clearInput(event) {
   document.querySelector('#guess').value = '';
   document.querySelector('#clearText').disabled = true;
   document.querySelector('#submit').disabled = true;
-  document.querySelector('#game').removeAttribute('class', 'hover');
   document.querySelector('#submit').setAttribute('class', 'buttonDisabled');
   document.querySelector('#clearText').setAttribute('class', 'buttonDisabled')
 }
