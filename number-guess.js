0d7d943f6e9206b669aa066195dc02be88996cc0
Jon Sweet <jonathan.h.sweet@gmail.com>
2017-12-04 10:55:31 -0700

    refactored js and fixed bug allowing use to guess with blank number field

--- number-guess.js
+++ number-guess.js
@@ -3,7 +3,7 @@
 
 // The input field should only accept numerical entries, within the defined min and max range - DONE - INPUT FIELD ONLY ACCEPTS NUMERICAL INPUT (changed type from 'submit' to 'number') BUT USER IS STILL ABLE TO MANUALLY TYPE NUMBER OUTSIDE OF MIN/MAX SET IN HTML.
 
-// The application should display an error message if the guess is not a number (e.g. parseInt() returns NaN). - DONR - USER IS UNABLE TO SUBMIT EMPTY FIELD OR NaN.
+// The application should display an error message if the guess is not a number (e.g. parseInt() returns NaN). - DONE - USER IS UNABLE TO SUBMIT EMPTY FIELD OR NaN.
 
 // The application should display an error if the guess is outside of the range of possible answers. - DONE - user is alert(ed) and description changed to instruct user to input guess between .max and .min. 
 
@@ -17,51 +17,81 @@ var guess = 0;
 var numberOfGuesses = 0;
 var ans = 0;
 var description = '';
+var winCase = 'You Win!<br />Click Reset to<br /> play again.';
 
 var sub = document.querySelector('#submit');
 var cl = document.querySelector('#clearText');
 var res = document.querySelector('#reset');
+
 
 sub.addEventListener('click', submitGuess);
 sub.addEventListener('click', gameOver);
 cl.addEventListener('click', clearInput);
 res.addEventListener('click', reset);
 
-// below - eventListener for user input into text area
-var userInput = document.querySelector('#guess');
-
-userInput.addEventListener('input', enableButtons, false);
-
-
 function enableButtons() {
   //function for eventListener 'input' in text box.
   if (!isNaN(this.value)){ 
-    document.querySelector('#submit').disabled = false;
-    document.querySelector('#clearText').disabled = false;
+    // document.querySelector('#submit').disabled = false;
+    toggleButtonOn('#submit');
+    // document.querySelector('#clearText').disabled = false;
+    toggleButtonOn('#clearText');
     changeAttribute('#game', 'class', 'hover');
-    changeAttribute('#submit', 'class', 'buttonEnabled');
-    changeAttribute('#clearText', 'class', 'buttonEnabled');
+    // changeAttribute('#submit', 'class', 'buttonEnabled');
+    // changeAttribute('#clearText', 'class', 'buttonEnabled');
   } else if (isNaN(this.value) || ((this.value > this.max) || (this.value < this.min))){
-    console.log('value isNaN line 39');
-    changeAttrubute('#submit', 'class', 'buttonDisabled');
-    changeAttribute('#clearText', 'class', 'buttonDisabled');
+    console.log('value isNaN');
+    // changeAttrubute('#submit', 'class', 'buttonDisabled');
+    toggleButtonOff('#submit')
+    // changeAttribute('#clearText', 'class', 'buttonDisabled');
+    toggleButtonOff('#clearText')
   }
 }
 
+var userInput = document.querySelector('#guess');
+userInput.addEventListener('input', enableButtons, false);
 
 function gameOver(event){
   if (description === 'BOOM!'){
-    changeAttribute('#submit', 'class', 'buttonDisabled');
-    document.querySelector('#submit').disabled = true;
-    document.querySelector('#const').innerHTML = 'BOOM!';
-    document.querySelector('#feedBack').innerHTML = 'You Win!<br />Click Reset to<br /> play again.';
+    toggleButtonOff('#submit');
+    // changeAttribute('#submit', 'class', 'buttonDisabled');
+    // document.querySelector('#submit').disabled = true;
+    changeText('#const', 'BOOM!');
+    // document.querySelector('#const').innerText = 'BOOM!';
+    changeHTML('#feedBack', winCase);
+    // document.querySelector('#feedBack').innerHTML = 'You Win!<br />Click Reset to<br /> play again.';
+    document.querySelector('#guess').disabled = true;
   }
 } 
 
 
 function changeAttribute(selector, attribute, value) { //function to change element attributes easier
-  toBeChanged = document.querySelector(selector);
+  var toBeChanged = document.querySelector(selector);
   toBeChanged.setAttribute(attribute, value);
+}
+
+function addClass(selector, value){
+  toBeChanged = document.querySelector(selector);
+  toBeChanged.classList.add(value);
+}
+
+function removeClass(selector, value){
+  toBeChanged = document.querySelector(selector);
+  toBeChanged.classList.remove(value);
+}
+
+function toggleButtonOn(buttonId){
+  console.log('toggleButtonOn: ' + buttonId);
+  removeClass(buttonId, 'buttonDisabled');
+  addClass(buttonId, 'buttonEnabled');
+  document.querySelector(buttonId).disabled = false;
+}
+
+function toggleButtonOff(buttonId){
+  console.log('toggleButtonOff: ' + buttonId);
+  removeClass(buttonId, 'buttonEnabled');
+  addClass(buttonId, 'buttonDisabled');
+  document.querySelector(buttonId).disabled = true;
 }
 
 function submitGuess(event) {
@@ -69,6 +99,8 @@ function submitGuess(event) {
   console.log('submit guess function');
   numberGuesser();
   event.preventDefault();
+  toggleButtonOff('#submit');
+  document.querySelector('#guess').disabled = true;
 }
 
 function numberGuesser(){
@@ -119,16 +151,25 @@ function getGuess(){
 
 function presentGuess() {
   console.log('guess presented');
-  document.querySelector('#attempt').innerHTML = guess;
+  changeText('#attempt', guess)
+  // document.querySelector('#attempt').innerText = guess;
   visElement('#attempt');
   visElement('#const');
   visElement('#reset');
 }
 
+function changeText(selector, text){
+  document.querySelector(selector).innerText = text;
+}
+
+function changeHTML(selector, text){
+  document.querySelector(selector).innerHTML = text;
+}
+
 function describeGuess(){
-  console.log('guess described')
-  document.querySelector('#feedBack').style.visibility = 'visible';
-  document.querySelector('#feedBack').innerHTML = description;
+  console.log('guess described');
+  visElement('#feedBack');
+  changeText('#feedBack', description);
   presentGuess();
 }
 
@@ -137,10 +178,13 @@ function clearInput(event) {
   console.log('Default form settings prevented');
   console.log('text box set to \'\'');
   document.querySelector('#guess').value = '';
-  document.querySelector('#clearText').disabled = true;
-  document.querySelector('#submit').disabled = true;
-  document.querySelector('#submit').setAttribute('class', 'buttonDisabled');
-  document.querySelector('#clearText').setAttribute('class', 'buttonDisabled')
+  toggleButtonOff('#clearText');
+  toggleButtonOff('#submit');
+  document.querySelector('#guess').disabled = false;
+  // document.querySelector('#clearText').disabled = true;
+  // document.querySelector('#submit').disabled = true;
+  // document.querySelector('#submit').setAttribute('class', 'buttonDisabled');
+  // document.querySelector('#clearText').setAttribute('class', 'buttonDisabled')
 }
 
 function reset(event) {
@@ -151,25 +195,26 @@ function reset(event) {
   invisElement('#const');
   invisElement('#reset');
   clearInput(event);
-  document.querySelector('#const').innerHTML = 'Your last guess was';
+  changeText('#const', 'Your last guess was');
+  document.querySelector('#guess').disabled = false;
 }
 
-function validateGuess(x) {
-  if (isNaN(x)) {
-    console.log('Please guess a number');
-  } else if (x > 100 || x < 1 || Number.isInteger(x) == false){
-    console.log('Please enter a whole number between 1 and 100.'); 
-  } else {
-    submitGuess();
-  } 
-}
+// function validateGuess(x) {
+//   if (isNaN(x)) {
+//     console.log('Please guess a number');
+//   } else if (x > 100 || x < 1 || Number.isInteger(x) == false){
+//     console.log('Please enter a whole number between 1 and 100.'); 
+//   } else {
+//     submitGuess();
+//   } 
+// }
 
 function visElement(element){
-  console.log('visElement function');
+  console.log('visElement: ' + element);
   document.querySelector(element).style.visibility = 'visible';
 }
 
 function invisElement(element){
-  console.log('invisElement function');
+  console.log('invisElement: ' + element);
   document.querySelector(element).style.visibility = 'hidden';
 }
